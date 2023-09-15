0057e158c4cdf39e7324edf06ec73841352af435
kei224022 <ishikawa224022@protonmail.com>
2023-09-15 16:41:32 +0900

    Initial Commit

---
+++ UWB_SwiftUI/UWB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UWB_SwiftUIApp.swift
+//  UWB_SwiftUI
+//
+//  Created by member on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct UWB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
